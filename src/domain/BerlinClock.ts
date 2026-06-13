f41ab84e75691f99bed751688a46bcb20f578717
Samad <tmelon.brand@gmail.com>
2026-06-13 19:47:15 +0200

    test: add Berlin clock tests (not implemented yet)

---
+++ src/domain/BerlinClock.ts
@@ -0,0 +1,9 @@
+import type { BerlinClockState, Time } from './types.ts'
+
+export function toBerlinClockState(_time: Time): BerlinClockState {
+  throw new Error('Not implemented')
+}
+
+export function toBerlinClockString(_time: Time): string {
+  throw new Error('Not implemented')
+}
